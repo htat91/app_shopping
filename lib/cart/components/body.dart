1f3f6286a479deddc7876cc86838fa304ee2b649
Lê Quốc Anh <66353897+lequocanh20@users.noreply.github.com>
2022-05-02 01:27:16 +0700

    Update body.dart
    update tap icon delete in body cart

--- lib/cart/components/body.dart
+++ lib/cart/components/body.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:app_shopping/model/carts.dart';
+import 'package:app_shopping/model/utilities.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:app_shopping/cart/components/checkoutcart.dart';
@@ -38,19 +39,35 @@ class _BodyState extends State<Body> {
               itemBuilder: (context, index)  {
               return Column(
                 children: [
-                  GestureDetector(
-                      child: CartItem(item: cartdetails[index],),
-                    onTap: (){
-                        setState(() {
-                          cartdetails.removeAt(index);
-                          sum = 0.0;
-                          cartdetails.forEach((item) { sum = sum + item.products!.price * item.quantity!; });
-                        });
-                    },
-                  ),
+                  Container(
+                        color: Color(0xFFF5F5F5),
+                        padding: EdgeInsets.all(16),
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: 100,
+                              height: 100,
+                              child: Image.asset(cartdetails[index].products!.image!)),
+                            Expanded(child: Text(cartdetails[index].products!.title)) ,
+                            Expanded(child: Text(cartdetails[index].products!.price.toStringAsFixed(3) + Utilities.getCurrency())),
+                            Expanded(child: Text(cartdetails[index].quantity.toString())),
+                            Expanded(child: GestureDetector(
+                              child: Icon(Icons.delete_outline),
+                                onTap: (){
+                                  setState(() {
+                                    cartdetails.removeAt(index);
+                                    sum = 0.0;
+                                    cartdetails.forEach((item) { sum = sum + item.products!.price * item.quantity!; });
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                   Divider()
                 ],
-              ) ;
+              );
             }),
           ),
           CheckOutCart(sum: sum.toStringAsFixed(3),)
@@ -60,35 +77,35 @@ class _BodyState extends State<Body> {
   }
 }
 
-class CartItem extends StatelessWidget {
-  Items item;
+// class CartItem extends StatelessWidget {
+//   Items item;
 
-  CartItem({required this.item});
+//   CartItem({required this.item});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xFFF5F5F5),
-      padding: EdgeInsets.all(16),
-      child: Row(
-        children: [
-          SizedBox(
-            width: 100,
-             height: 100,
-              child: Image.asset(item.products!.image!)),
-          Expanded(child: Text(item.products!.title)) ,
-          Expanded(child: Text(item.products!.price.toStringAsFixed(3) + getCurrency())),
-          Expanded(child: Text(item.quantity.toString())),
-          Icon(Icons.delete_outlined)
-        ]
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       color: Color(0xFFF5F5F5),
+//       padding: EdgeInsets.all(16),
+//       child: Row(
+//         children: [
+//           SizedBox(
+//             width: 100,
+//              height: 100,
+//               child: Image.asset(item.products!.image!)),
+//           Expanded(child: Text(item.products!.title)) ,
+//           Expanded(child: Text(item.products!.price.toStringAsFixed(3) + getCurrency())),
+//           Expanded(child: Text(item.quantity.toString())),
+//           Icon(Icons.delete_outlined)
+//         ]
 
-      ),
-    );
-  }
-}
+//       ),
+//     );
+//   }
+// }
 
-String getCurrency() {
-  Intl.defaultLocale = 'vi_VN';
-  var format = NumberFormat.simpleCurrency(locale: Intl.defaultLocale);
-  return format.currencySymbol;
-}
+// String getCurrency() {
+//   Intl.defaultLocale = 'vi_VN';
+//   var format = NumberFormat.simpleCurrency(locale: Intl.defaultLocale);
+//   return format.currencySymbol;
+// }
